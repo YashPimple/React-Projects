8e7e7d05d9599f40a18ca82aaa2fca2034192101
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2022-10-22 09:22:44 +0530

    Update app.js

--- 05-To-do-list/Project-05/app.js
+++ 05-To-do-list/Project-05/app.js
@@ -9,6 +9,7 @@ let items = ["But food", "Cook food","Eat food"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req,res){ 
 
